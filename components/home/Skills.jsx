c1705d1853cf00b8d5e3882adbf4e28f6d988e03
mrprotocoll <lekanvgbg@gmail.com>
2023-06-09 19:48:36 +0100

    Add tools to db

--- components/home/Skills.jsx
+++ components/home/Skills.jsx
@@ -17,11 +17,11 @@ const Skills = () => {
         }, []
     )
     return (
-        <div data-aos="zoom-in" className="flex-1">
+        <div data-aos="zoom-in" className="">
             <Card classes="about-profile-box" link="/about">
                 <ul className="d-flex flex-wrap gap-2 mb-4">
                     {
-                        skills.technical.map((skill) => (
+                        skills.map((skill) => (
                             <li><button className="badge">{skill}</button></li>
                         ))
                     }
