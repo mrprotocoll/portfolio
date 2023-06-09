9449bc0e11cd579881672d3d3fe18b1aaf01c387
mrprotocoll <lekanvgbg@gmail.com>
2023-06-09 20:00:47 +0100

    fetched tools from api

--- components/home/Tools.jsx
+++ components/home/Tools.jsx
@@ -2,33 +2,33 @@ import Card from "@components/Card";
 import CardInfo from "@components/CardInfo";
 import Image from "next/image";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 const Tools = () => {
+    const [tools, setTools] = useState([])
+
+    useEffect(
+        () => {
+            (async () => {
+                const res = await fetch("/db/bio.json")
+                const resData = await res.json()
+                setTools(resData.tools)
+            })()
+        }, []
+    )
+
     return (
         <div data-aos="zoom-in" className="">
             <Card classes="about-profile-box" link="/blogs">
                 <div className="inner-profile-icons shadow-box flex-wrap">
-                    <Link href="#">
-                        <Image src="/assets/images/tech/javascript.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
-                    <Link href="#">
-                        <Image src="/assets/images/tech/docker.png" alt="Button" width={46} height={42} />
-                    </Link>
+                    {
+                        tools.map((tool) => (
+                            <Link href={tool.link}>
+                                <Image src={tool.image} alt={tool.name} width={46} height={42} />
+                            </Link>
+                        ))
+                    }
+                  
                 </div>
 
                 <div className="d-flex align-items-center justify-content-between">
