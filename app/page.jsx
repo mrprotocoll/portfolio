0ce03a0f70e102424db6dff5756fb8b80c355545
mrprotocoll <lekanvgbg@gmail.com>
2023-06-09 22:13:53 +0100

    refactored home page to use dynamic metadata

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,3 @@
-'use client'
-
-import Aos from "aos";
-import { useEffect, useState } from 'react';
 import AboutCard from '@components/home/AboutCard'
 import Marquee from "@components/home/Marquee";
 import Credential from "@components/home/Credential";
@@ -11,29 +7,17 @@ import Tools from "@components/home/Tools";
 import Skills from "@components/home/Skills";
 import Contact from "@components/home/Contact";
 
+export const metadata = {
+  title: 'Home'
+}
+
 export default function Home() {
-  const [data, setData] = useState({})
 
-  useEffect(() => {
-    Aos.init({
-      easing: "ease-out-cubic",
-      once: true,
-      offset: 50,
-    });
-
-    // get profile data 
-    (async () => {
-      const response = await fetch("/db/bio.json")
-      const responseData = await response.json()
-
-      setData(responseData)
-    })()
-  }, []);
   return (
     <section className="about-area">
         <div className="row">
           <div className="col-md-6" data-aos="zoom-in">
-            <AboutCard data={data}/>
+            <AboutCard />
           </div>
 
           <div className="col-md-6">
