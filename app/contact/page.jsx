eec6084c7bd80d292671105d5107b7182c63dc12
mrprotocoll <lekanvgbg@gmail.com>
2023-06-12 16:20:28 +0100

    Add contact styling

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,5 +1,6 @@
 import CardOverlay from "@components/CardOverlay"
 import Form from "@components/contact/Form"
+import { Twitter, GitHub, LinkedIn, Medium, Mail } from 'iconoir-react'
 
 export const metadata = {
   title: 'Contact'
@@ -16,42 +17,22 @@ export default function Contact() {
                 <ul className="contact-details">
                     <li className="d-flex align-items-center" data-aos="zoom-in">
                         <div className="icon-box shadow-box">
-                            <i className="iconoir-mail"></i>
+                            <Mail />
                         </div>
                         <div className="right">
-                            <span>MAIL us</span>
-                            <h4>info@bluebase.com</h4>
-                            <h4>info@bluebase2.com</h4>
-                        </div>
-                    </li>
-
-                    <li className="d-flex align-items-center" data-aos="zoom-in">
-                        <div className="icon-box shadow-box">
-                            <i className="iconoir-phone"></i>
-                        </div>
-                        <div className="right">
-                            <span>Contact Us</span>
-                            <h4>+1 504-899-8221</h4>
-                            <h4>+1 504-749-5456</h4>
-                        </div>
-                    </li>
-
-                    <li className="d-flex align-items-center" data-aos="zoom-in">
-                        <div className="icon-box shadow-box">
-                            <i className="iconoir-pin-alt"></i>
-                        </div>
-                        <div className="right">
-                            <span>Location</span>
-                            <h4>22 Baker Street, Texas <br />United States <br/>W1U 3BW</h4>
+                          <span>MAIL me</span>
+                          <a href="mailto:mrprotocoll@yahoo.com" className="text-white">mrprotocoll@yahoo.com</a>
                         </div>
                     </li>
                 </ul>
 
                 <h3 data-aos="fade-up">Social Info</h3>
                 <ul className="social-links d-flex align-center" data-aos="zoom-in">
-                    <li><a className="shadow-box" href="#"><i className="iconoir-dribbble"></i></a></li>
-                    <li><a className="shadow-box" href="#"><i className="iconoir-twitter"></i></a></li>
-                    <li><a className="shadow-box" href="#"><i className="iconoir-instagram"></i></a></li>
+                      <li><a className="shadow-box" target="_blank" href="https://www.linkedin.com/in/mrprotocoll"><LinkedIn /></a></li>
+                        <li><a  className="shadow-box"target="_blank" href="https://twitter.com/dprotocoll"><Twitter /></a></li>
+                        <li><a className="shadow-box" target="_blank" href="https://github.com/mrprotocoll"><GitHub /></a></li>
+                       
+                        <li><a className="shadow-box" target="_blank" href="https://medium.com/@mrprotocoll"><Medium /></a></li>
                 </ul>
             </div>
 
@@ -59,7 +40,7 @@ export default function Contact() {
                 <div className="shadow-box">
                   <CardOverlay />
                   <img src="assets/images/icon3.png" alt="Icon" />
-                  <h1>Let’s work <span>together.</span></h1>
+                  <h1 className="text-light">Let’s work <span>together.</span></h1>
                   <Form />
                 </div>
             </div>
