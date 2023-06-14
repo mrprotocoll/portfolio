a2928f3eef506486df2c28908a93c5939ee72ec0
mrprotocoll <lekanvgbg@gmail.com>
2023-06-14 08:21:36 +0100

    Add projects from db

--- components/project/Projects.jsx
+++ components/project/Projects.jsx
@@ -4,10 +4,11 @@ import Aos from "aos"
 import Card from "@components/Card";
 import CardInfo from "@components/CardInfo";
 import Image from "next/image";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 const Projects = () => {
+    const [projects, setProjects] = useState([])
 
     useEffect(() => {
         Aos.init({
@@ -15,20 +16,33 @@ const Projects = () => {
             once: true,
             offset: 50,
         });
+
+        (async () => {
+            const res = await fetch("/db/projects.json")
+            const resData = await res.json()
+            setProjects(resData)
+        })()
+
     }, [])
 
     return (
         <div class="row align-items-start flex-wrap">
-          <div data-aos="zoom-in" class="col-md-4">
-            <Card classes="project-item">
-                <div class="project-img">
-                  <Image src="/assets/images/projects/healthyish2.png" alt="Sign" fill />
-                </div>
-                <div className="d-flex align-items-center justify-content-between">
-                    <CardInfo name="MOBILE DESIGNING" title="Seven Studio" />
-                </div>
-            </Card>
-          </div>
+
+            {
+                projects.map( project => (
+                    <div data-aos="zoom-in" class="col-md-4">
+                        <Card classes="project-item">
+                            <div class="project-img">
+                            <Image src={project.image} alt={project.name} fill />
+                            </div>
+                            <div className="d-flex align-items-center justify-content-between">
+                                <CardInfo name={project.category} title={project.name}  link={`/project-details?id=${project.id}`} />
+                            </div>
+                        </Card>
+                    </div>
+                ))
+            }
+          
         </div>
     )
 }
