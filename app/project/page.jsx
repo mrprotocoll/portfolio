837b5a92d0c3d3fc4670d60ffced5e4307b76fdc
mrprotocoll <lekanvgbg@gmail.com>
2023-06-14 08:21:36 +0100

    Add projects from db

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -10,8 +10,8 @@ export default function Project() {
   return (
     <section className="projects-area">
       <div class="container">
-        <h1 class="section-heading">
-          <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /> All Projects <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /></h1>
+        <h1 class="section-heading mb-5 text-light">
+          <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /> My Projects <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /></h1>
 
         <Projects />
 
