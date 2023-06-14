82867e68d468167f1ca329b6c34c320b9d67fc27
mrprotocoll <lekanvgbg@gmail.com>
2023-06-14 07:44:59 +0100

    Add projects component to project

--- app/project/page.jsx
+++ app/project/page.jsx
@@ -1,3 +1,6 @@
+import Projects from "@components/project/Projects"
+import Image from "next/image"
+
 export const metadata = {
   title: 'Projects'
 }
@@ -6,7 +9,13 @@ export default function Project() {
 
   return (
     <section className="projects-area">
-      <h1>My Projects</h1>
+      <div class="container">
+        <h1 class="section-heading">
+          <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /> All Projects <Image src="/assets/images/star-2.png" alt="Star" width={48} height={48} /></h1>
+
+        <Projects />
+
+      </div>
     </section>
   )
 }
